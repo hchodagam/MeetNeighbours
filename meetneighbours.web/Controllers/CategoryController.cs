237d8f7166216b29e8c3b4e2a804c0aa0d1af035
hchodagam <harikrishnach.net@gmail.com>
2015-05-13 21:57:59 +0530

    web checkin

---
+++ meetneighbours.web/Controllers/CategoryController.cs
@@ -0,0 +1,50 @@
+﻿using meetneighbours.DTO;
+using meetneighbours.services.contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace meetneighbours.web.Controllers
+{
+    public class CategoryController : ApiController
+    {
+        private ICategoryService _CategoryService;
+        private IBusinessListService _BusinessListService;
+        public CategoryController(ICategoryService CategoryService, IBusinessListService BusinessListService)
+        {
+            this._CategoryService = CategoryService;
+            this._BusinessListService = BusinessListService;
+        }
+        // GET: api/Category
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Category/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Category
+        public void Post([FromBody]Category _Category)
+        {
+           _CategoryService.AddCategory(_Category);
+           // _BusinessListService.AddBusinessList(_BusinessLists);
+        }
+
+        // PUT: api/Category/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Category/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ meetneighbours.web/Scripts/ClientScripts/maincontroller.js
@@ -0,0 +1,18 @@
+﻿var app = angular.module('MeetNeighbours', []);
+
+
+
+app.controller('MeetNeighboursController', function ($scope, $http, $filter, $location) {
+    $scope.add = function () {
+       
+        $http.post('api/Category', this.Category).success(function (data) {
+            alert("Added Successfully!!");
+
+        }).error(function (data) {
+            $scope.error = "An Error has occured while Adding Friend! " + data;
+            $scope.loading = false;
+
+        });
+    };
+
+});
